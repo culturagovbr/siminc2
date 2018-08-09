eea5a1da335a9dcd66666c8d7f30196457906e8a
ramonsales <ramon.silvasales@gmail.com>
2018-08-09 18:12:01 -0300

    [UPDATE]Docker Compose. Inserindo os arquivos de configuração do siminc2 e atualizando o docker-compose.yml file.

---
+++ global/database-exemplo.php
@@ -0,0 +1,17 @@
+<?php
+
+# Base de dados do sistema SIMINC2
+$nome_bd = 'siminc2_desenvolvimento';
+$servidor_bd = 'localhost';
+$porta_bd = '5452';
+$usuario_db = 'usr_simec';
+$senha_bd = '123456';
+
+# Base de dados do sistema SIMINC antigo
+$configDbOldSiminc = new stdClass();
+$configDbOldSiminc->host = '[PREENCHER]';
+$configDbOldSiminc->port = '[PREENCHER]';
+$configDbOldSiminc->dbname = '[PREENCHER]';
+$configDbOldSiminc->user = '[PREENCHER]';
+$configDbOldSiminc->password = '[PREENCHER]';
+$configDbOldSiminc->clientEncoding = 'ISO-8859-1';
--- sql/restore/desenvolvimento.sh
+++ sql/restore/desenvolvimento.sh
@@ -1,11 +1,7 @@
+#!/bin/bash
+
 psql --port 5432 --username "postgres" --dbname "siminc2_desenvolvimento" -f /var/www/db-init/users.sql
 
 psql --port 5432 --username "postgres" -c "ALTER DATABASE siminc2_desenvolvimento SET datestyle TO European;"
 
 pg_restore --port 5432 --username "postgres" --dbname "siminc2_desenvolvimento" --disable-triggers -O -x --verbose /var/www/db-init/bkp_prod_dbsiminc.backup
-
-# Executar cria��o de estrutura de auditoria e mudan�a de senhas, emails
-psql --port 5432 --username "postgres" --dbname "siminc2_desenvolvimento" -f /var/www/db-init/create_auditoria.sql
-
-# Executar permiss��es nas tabelas
-psql --port 5432 --username "postgres" --dbname "siminc2_desenvolvimento" -f /var/www/db-init/grants.sql
