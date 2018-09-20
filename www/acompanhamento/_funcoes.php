3d8427ed541760fab6e77b5201216f58bd5a7a37
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2018-09-20 11:59:01 -0300

    [UPDATE]Gerador de relatórios-Relatório de Funcionais. Issue #521
    Criando filtros padrões do relatório e criando lista de visualização do relaorio e exportação em planilha.

--- www/acompanhamento/_funcoes.php
+++ www/acompanhamento/_funcoes.php
@@ -6,7 +6,8 @@
      * @param $filtros
      * @return VOID
      */
-    function montarComboUG(stdClass $filtros) {
+    function montarComboUG(stdClass $filtros)
+    {
         global $simec;
 
         return $simec->select(
@@ -16,4 +17,59 @@
             Public_Model_SubUnidadeOrcamentaria::queryCombo((object) array(
                 'exercicio' => $filtros->exercicio,
                 'unicod' => $filtros->unicod)));
+    }
+
+    /**
+     * Monta configuração de nome e descricao de colunas para o relatório Gerencial de Funcionais.
+     * 
+     * @return array
+     */
+    function montarColunasRelatorioFuncional()
+    {
+        $colunas = array(
+            array(
+                'codigo' => 'unidade',
+                'descricao' => 'Unidade'),
+            array(
+                'codigo' => 'subunidade',
+                'descricao' => 'Subunidade'),
+            array(
+                'codigo' => 'ptres',
+                'descricao' => 'PTRES'),
+            array(
+                'codigo' => 'funcional',
+                'descricao' => 'Funcional'),
+            array(
+                'codigo' => 'custeio_dotacao',
+                'descricao' => 'Dotação/Custeio'),
+            array(
+                'codigo' => 'custeio_planejado',
+                'descricao' => 'Planejado/Custeio'),
+            array(
+                'codigo' => 'capital_dotacao',
+                'descricao' => 'Dotação/Capital'),
+            array(
+                'codigo' => 'capital_planejado',
+                'descricao' => 'Planejado/Capital'),
+            array(
+                'codigo' => 'total_dotacao',
+                'descricao' => 'Dotação/Total'),
+            array(
+                'codigo' => 'total_planejado',
+                'descricao' => 'Planejado/Total'),
+            array(
+                'codigo' => 'provisionado',
+                'descricao' => 'Provisionado'),
+            array(
+                'codigo' => 'empenhado',
+                'descricao' => 'Empenhado'),
+            array(
+                'codigo' => 'liquidado',
+                'descricao' => 'Liquidado'),
+            array(
+                'codigo' => 'pago',
+                'descricao' => 'Pago'),
+        );
+        
+        return $colunas;
     }
