ff16dee12c55ee6c25767ee8d1cb31677abdce0c
Douglasx <douglasantana007@gmail.com>
2018-10-22 12:23:43 -0300

    [FIX]Primeiro Acesso-Liberação de usuários email. Issue #649

--- includes/cadastro_usuario_geral.php
+++ includes/cadastro_usuario_geral.php
@@ -519,7 +519,7 @@ function gerFuncResp ($sisid, $sisDir, $usucpf, $dados = array()) {
 			if ( $email_aprovacao) {
 				$remetente = array("nome" => $_SESSION['usunome'],"email" => $_SESSION['usuemail']);
 				$destinatario = $_REQUEST['usuemail'];
-				$assunto = "Aprovação do Cadastro no Simec";
+                $assunto = "Aprovação do Cadastro no ".SIGLA_SISTEMA;
 				$conteudo = "
 					<br/>
 					<span style='background-color: red;'><b>Esta é uma mensagem gerada automaticamente pelo sistema. </b></span>
