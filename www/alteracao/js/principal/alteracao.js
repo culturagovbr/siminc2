58bc4e39e144b428e8e8924234d29499b370b2a3
Douglasx <douglasantana007@gmail.com>
2018-10-24 08:01:24 -0300

    [ADD]Alterações - Lista Alterações. Issue #502
    Adicionado mock na lista de alterações orçamentárias, personalizado os campos de pesquisa avançada.

---
+++ www/alteracao/js/principal/alteracao.js
@@ -0,0 +1,17 @@
+
+/**
+ * Ações efetuadas quando a tela de lista de Alterações Orçamentárias é iniciada.
+ *
+ */
+function initListaAlteracao() {
+
+    $('.btn-limpar').click(function(){
+        $('#requisicao').val('limpar');
+        $('#filtroalteracao').submit();
+    });
+
+    $("#btn-exportar-xls").click(function(){
+//            window.open('?modulo=inicio&acao=C&req=lista-inicio-xls&tipo='+ $("#tipo").val());
+    });
+
+}
