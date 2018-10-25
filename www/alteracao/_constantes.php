f532e5e063196a58ec77bb1e6a330dd37234dde7
Douglasx <douglasantana007@gmail.com>
2018-10-25 10:16:09 -0300

    [ADD] Alterações - Model de Workflow.
    Adicionado a modelo de workflow.estadodocumento para recuperar os estados no combo usado no módulo.

--- www/alteracao/_constantes.php
+++ www/alteracao/_constantes.php
@@ -1,11 +1,19 @@
 <?php
+/**
+ * Constantes do Módulo Alterações Orçamentárias.
+ * @author $douglas santana fontes <douglas.fontes@cultura.gov.br>
+ * @since 25-10-2018
+ *
+ */
+
 define('SISID_PLANEJAMENTO', 256);
 
 define('PFL_SUPERUSUARIO', 1512);
 define("PFL_SUBUNIDADE", 1513);
 define("PFL_ADMINISTRADOR", 1514);
 
-define("WF_TPDID_PREPI", 268);
+define("WF_TPDID_INTERNO", 269);
+define("WF_TPDID_EXTERNO", 270);
 
 define("ESD_PREPI_CADASTRAMENTO", 1790);
 define("ESD_PREPI_AGUARDANDO_APROVACAO", 1789);
