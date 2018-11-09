2ca7e523897f9e6c881ca2c95912621d116a88b1
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2018-11-09 12:54:17 -0200

    [FIX]WEBSERVICE SIOP-EXECUÇÃO ORÇAMENTÁRIA. Issue #727
    Corrigindo caminho físico de arquivos.

--- includes/siop-soap-curl/siop-soap-curl.php
+++ includes/siop-soap-curl/siop-soap-curl.php
@@ -1,20 +1,17 @@
 <?php
 
 # Cliente do Webservice
-include_once 'soap-curl/client.php';
+include_once APPRAIZ. 'includes/soap-curl/client.php';
 
 # Contrato de classes
-include_once 'interface-service.php';
-include_once 'interface-xml.php';
-
-# Cliente do Webservice
-include_once 'soap-curl/client.php';
+include_once APPRAIZ. 'includes/siop-soap-curl/interface-service.php';
+include_once APPRAIZ. 'includes/siop-soap-curl/interface-xml.php';
 
 # Classes abstratas pra implementação de serviços
-include_once 'siop-soap-curl/service.php';
-include_once 'siop-soap-curl/xml.php';
+include_once APPRAIZ. 'includes/siop-soap-curl/service.php';
+include_once APPRAIZ. 'includes/siop-soap-curl/xml.php';
 
 # Classes personalizadas para consumir serviços
-include_once 'siop-soap-curl/quantitativo/service/execucao-orcamentaria.php';
-include_once 'siop-soap-curl/quantitativo/xml/execucao-orcamentaria.php';
+include_once APPRAIZ. 'includes/siop-soap-curl/quantitativo/service/execucao-orcamentaria.php';
+include_once APPRAIZ. 'includes/siop-soap-curl/quantitativo/xml/execucao-orcamentaria.php';
 
