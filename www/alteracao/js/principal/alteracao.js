5d20da96d563c65fe09d288c92555517cacce806
Douglasx <douglasantana007@gmail.com>
2018-11-26 14:26:14 -0200

    [ADD] Alterações - Lista de Pedido de Alterações Orçamentárias.
    Adicionado ajax para trazer lista de internos e externos de acordo com o clique do botão da tabs.

--- www/alteracao/js/principal/alteracao.js
+++ www/alteracao/js/principal/alteracao.js
@@ -10,8 +10,20 @@ function initListaAlteracao() {
         $('#filtroalteracao').submit();
     });
 
-    $("#btn-exportar-xls").click(function(){
+    $('#btn-exportar-xls').click(function(){
 //            window.open('?modulo=inicio&acao=C&req=lista-inicio-xls&tipo='+ $("#tipo").val());
+    });
+
+    $('.tab-tipo').click(function () {
+
+        $.ajax({
+            type: 'POST',
+            url: '?modulo=inicio&acao=C&requisicao=lista-pedidos&tpdid='+ $(this).attr('tipo') +'&dados='+ $('#filtroalteracao').serialize(),
+            success: function(dados){
+                $('#div-principal').html(dados);
+            }
+        });
+
     });
 
 }
