84eb39e181d81622cc1bb287d4836699296d8569
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2018-11-27 15:23:35 -0200

    [REFACTORY]Painel FNC-Execução Orçamentária. Issue #796
    Retirando funções não utilizadas, colocando arquivo de javascript externo.

---
+++ www/planacomorc/js/painel.js
@@ -0,0 +1,65 @@
+
+/**
+ * Atualiza os dados de execução orçamentária
+ * 
+ * @returns VOID Retorna janela modal com aviso pra o usuário
+ */
+function atualizarSIOP(){
+    divCarregando();
+    $.ajax({
+        type: 'POST',
+        url: window.location.href,
+        data: 'req=atualizarSIOP',
+        dataType: 'json',
+        success: function(resp){
+            if(resp.success){
+                swal({
+                        title: '',
+                        text: resp.message,
+                        type: 'success',
+                        showCancelButton: false,
+                        confirmButtonText: 'OK',
+                        closeOnConfirm: true
+                    },
+                    function(){
+                        window.location.href = window.location.href;
+                    });
+            } else {
+                swal({
+                        title: 'Erro!',
+                        text: resp.message,
+                        type: 'error',
+                        showCancelButton: false,
+                        confirmButtonText: 'OK',
+                        closeOnConfirm: true
+                    },
+                    function(){
+                        window.location.href = window.location.href;
+                    });
+            }
+        }
+    });
+}
+
+/**
+ * Exibe o modal com grafico detalhado da UO e Subunidade
+ * 
+ * @param {int} tipoGraficoSubunidade
+ * @param {string} tipoValor
+ * @param {string} sigla
+ * @returns VOID Exibe janela modal com o grafico
+ */
+function exibirModalDetalheGrafico(tipoGraficoSubunidade, tipoValor, sigla){
+    var urlBase = 'planacomorc.php?modulo=inicio&acao=C';
+    if(tipoGraficoSubunidade === 1){
+        var parametroTipoGrafico = '&req=detalhe-grafico-subunidade';
+    } else {
+        var parametroTipoGrafico = '&req=detalhe-grafico-uo';
+    }
+    
+    // Dotação ou Planejado
+    if(tipoValor.substr(0, 3) === "Dot" || tipoValor.substr(0, 3) === "Pla"){
+        $('#detalhe-grafico .modal-body').load(urlBase+ '&tipo_valor='+ tipoValor.substr(0, 3)+ '&sigla='+ sigla+ parametroTipoGrafico);
+        $('#detalhe-grafico').modal();
+    }
+}
