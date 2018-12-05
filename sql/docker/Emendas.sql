b513c4da4cbcc958629a039ba261a689ab771e07
Douglasx <douglasantana007@gmail.com>
2018-12-05 12:10:48 -0200

    [FIX] Docker Compose - Módulo Emendas. Issue #750
    Feito correções e testado aplicação com base de dados limpa.

---
+++ sql/docker/Emendas.sql
@@ -0,0 +1,14 @@
+/*
+ * Arquivo de Remoção de dados das tabelas domínios do Sistema.
+ *
+ * @since 2018/12/05
+ * @author Douglas Santana <douglas.fontes@cultura.gov.br>
+ */
+
+UPDATE emendas.emenda SET emestatus = 'I';
+UPDATE public.subunidadeorcamentaria SET suostatus = 'I';
+UPDATE public.unidadeorcamentaria SET unostatus = 'I';
+UPDATE emendas.beneficiario SET benstatus = 'I';
+UPDATE emendas.beneficiariodetalhe SET bedstatus = 'I';
+DELETE FROM emendas.usuario_siconv;
+UPDATE emendas.proponente SET prostatus = 'I';
