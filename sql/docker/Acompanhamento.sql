cd8997b3cad385c5e678ed09bef057ba8e0bbc2b
Douglasx <douglasantana007@gmail.com>
2018-12-05 16:33:11 -0200

    [FIX] Docker Compose - Módulo Monitoramento. Issue #787
    Adicionado script de banco para limpar os dados da aplicação tornando-a como sistema novo.

---
+++ sql/docker/Acompanhamento.sql
@@ -0,0 +1,7 @@
+/*
+ * Arquivo de Remoção de dados das tabelas domínios do Sistema SIMINC2 - Módulo Acompanhamento (Monitoramento).
+ *
+ * @since 2018/12/05
+ * @author Douglas Santana <douglas.fontes@cultura.gov.br>
+ */
+
