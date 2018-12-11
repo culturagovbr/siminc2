a01b6e62ec65c7320a7db6b84cbc22bf0b32527a
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2018-12-11 17:47:14 -0200

    [UPDATE]Docker Compose. Issue #750
    Corrigindo configuração de fuso horario da base de dados.

--- sql/restore/desenvolvimento.sh
+++ sql/restore/desenvolvimento.sh
@@ -2,6 +2,6 @@
 
 psql --port 5432 --username "postgres" --dbname "siminc2_desenvolvimento" -f /var/www/db-init/users.sql
 
-psql --port 5432 --username "postgres" -c "ALTER DATABASE siminc2_desenvolvimento SET datestyle TO European;"
+psql --port 5432 --username "postgres" -c "ALTER DATABASE siminc2_desenvolvimento SET datestyle TO European; ALTER DATABASE siminc2_desenvolvimento SET timezone TO 'America/Sao_Paulo';"
 
 pg_restore --port 5432 --username "postgres" --dbname "siminc2_desenvolvimento" --disable-triggers --verbose /var/www/db-init/siminc2_desenvolvimento.bkp
