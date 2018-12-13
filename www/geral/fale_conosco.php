aec2790238ec8926c008c44e75df196d0bb656d0
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2018-12-13 10:34:15 -0200

    [FIX]Retirando tratamento de acesso restrito a autenticação.

--- www/geral/fale_conosco.php
+++ www/geral/fale_conosco.php
@@ -4,9 +4,6 @@
     include_once APPRAIZ."includes/funcoes.inc";
     include_once APPRAIZ."includes/classes_simec.inc";
     
-    # Verifica se a sessão não expirou, se tiver expirada envia pra tela de login.
-//    controlarAcessoSemAutenticacao();
-    
     include_once APPRAIZ . "includes/Email.php";
 
     $db = new cls_banco();
