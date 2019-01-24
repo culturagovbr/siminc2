9ca4ca357828bdc1ec6b5174da4819cea38fbd8a
Douglasx <douglasantana007@gmail.com>
2019-01-24 16:46:46 -0200

    [ADD] Alterações Orçamentárias - Lista Principal.
    Feito melhoria no arquivo de javascript e adicionado alerta de confirmação de exclusão de pedido de alteração orçamentária.

--- www/alteracao/js/principal/alteracao.js
+++ www/alteracao/js/principal/alteracao.js
@@ -19,5 +19,49 @@ function initListaAlteracao() {
         $('#filtroalteracao').submit();
     });
 
+    $('body').on('click', '.a_espelho_pedido', function(){
+        var pedid = $(this).attr('data-pedid');
+        exibirEspelhoPedido(pedid);
+        return false;
+    });
+
+    $('.btn-excluir-pedido').click(function () {
+        var pedid = $(this).data('pedid');
+        removerPedido(pedid);
+    });
 }
 
+/**
+ * Exibe popup com Detalhes do Pedido. Tela de Espelho de Pedido.
+ *
+ * @returns VOID
+ */
+function exibirEspelhoPedido(pedid){
+    window.open(
+        window.location.href + '&req=espelho-pedido&pedid='+ pedid,
+        'popup_espelho_pedido',
+        'width=980,height=1000,status=1,menubar=1,toolbar=0,scrollbars=1,resizable=1');
+}
+
+/**
+ * Exclui o pedido de Alteração Orçamentária.
+ *
+ * @param pedid
+ */
+function removerPedido(pedid) {
+    swal({
+            title: "Atenção!",
+            text: "Deseja realmente excluir o registro ?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Sim, estou certo!",
+            closeOnConfirm: false
+        },
+        function(isConfirm){
+
+            if (isConfirm){
+                window.location.href = 'alteracao.php?modulo=inicio&acao=C&requisicao=excluir_pedido&pedid='+pedid
+            }
+        });
+}
