7a1538dcbdeff30b01d259f26f29bc93cc8bfba3
douglas <douglasantana007@gmail.com>
2019-04-29 09:17:59 -0300

    [FIX]Planejamento-Correção de Dotações.
    Aumentado o tempo de espera da atualização de dotação.

--- includes/siop-soap-curl/service.php
+++ includes/siop-soap-curl/service.php
@@ -102,7 +102,7 @@ abstract class SiopSoapCurl_Service implements SiopSoapCurl_InterfaceService {
             ->setWsdl($this->url. '?wsdl')
             ->setReturn(TRUE)
             ->setAuth(CURLAUTH_ANY)
-            ->setTimeout(180)
+            ->setTimeout(360)
             ->setPost(TRUE)
             ->setListHeader($this->mountListHeader())
         ;
